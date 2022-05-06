fa80a11684b755d18259c33acf833c8a39d27c5d
MS262010 <84782468+MS262010@users.noreply.github.com>
2022-05-06 15:51:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+marioX = 325;
+marioY = 325;
+
+function preload() {
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_kick=loadSound("kick.wav");
+	mario_die=loadSound("mariodie.wav");
+	world_start = loadSound("world_start.wav");
+	mario_gameover=loadSound("gameover.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240, 336);
+	canvas.parent('canvas');
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent("game_console");
+	instializeInSetup(mario);;
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game();
+	
+}
+function modelLoaded() {
+	console.log('Model Initialized!');
+}
+function gotPoses(results) {
+	if(results.length>0){
+	noseX = results[0].pose.nose.x;
+	noseY = results[0].pose.nose.y;
+	}
+}
+
+
+
+
+
+
